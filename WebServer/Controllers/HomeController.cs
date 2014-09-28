910155cc8b6a627c92103fb85902ef9cbccb666a
irotaev <irotaev@gmail.com>
2014-09-29 00:06:28 +0400

    Основа проекта. Первый этап.

---
+++ WebServer/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebServer.Controllers
+{
+  public class HomeController : AbstractController
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ WebServer/Scripts/Pages/Home.ts
@@ -0,0 +1,8 @@
+﻿/// <reference path="../typings/requirejs/require.d.ts" />
+
+import requireJSinitModule = require('../Framework/requireJsInit');
+var angular = require(['angular']);
+
+requireJSinitModule.RequireJsConfig.Init();
+
+console.log(angular);
